41edcc5d31c760ed8a520fa1de92e5496a32e372
pdtieubinh <tinybottles2112@gmail.com>
2023-08-29 17:27:11 +0300

    add frontend routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<p>Layout<Outlet/></p>}>
+                    <Route index element={<p>Projects</p>} />
+                    <Route path="login" element={<p>Login</p>} exact/>
+                    <Route path="*" element={<p>404: Error</p>} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
---
+++ src/services/project.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+const baseUrl = '/api/projects'
+
+let token = null
+const setToken = newToken => token = `Bearer ${newToken}`
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = async project => {
+    const config = {
+        headers: { Authorization: token }
+    }
+    const response = await axios.post(baseUrl, project, config)
+    return response.data
+}
+
+const edit = async (id, project) => {
+    const config = {
+        headers: { Authorization: token }
+    }
+    const response = await axios.put(`${baseUrl}/${id}`, project, config)
+    return response.data
+}
+
+const remove = async id => {
+    const config = {
+        headers: { Authorization: token }
+    }
+    const response = await axios.delete(`${baseUrl}/${id}`, config)
+    return response.data
+}
+
+export default { getAll, create, edit, remove, setToken }
