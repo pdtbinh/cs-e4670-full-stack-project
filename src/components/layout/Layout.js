b1b74090e82e9af8cd6be3a32c119594a1f5cf01
pdtieubinh <tinybottles2112@gmail.com>
2023-09-01 22:33:39 +0300

    style layout navbar

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,6 +1,9 @@
 import { NavLink } from 'react-router-dom'
 import { setToken } from '../../services/project'
 import { localStorageKey } from '../../keys/keywords'
+import LoginIcon from '@mui/icons-material/Login'
+import GroupWorkIcon from '@mui/icons-material/GroupWork'
+import './Layout.css'
 
 const Layout = ({ user, setUser, children }) => {
 
@@ -11,16 +14,22 @@ const Layout = ({ user, setUser, children }) => {
     }
 
     return (
-        <div>
-            {user ?
-                <>
-                    <p>{`User: ${user.name}`}</p>
-                    <button onClick={handleLogout}>Logout</button>
-                </>
-                : <NavLink to='/login'>
-                    <button>Login</button>
-                </NavLink>
-            }
+        <div className='Layout'>
+            <div className='LayoutPanel'>
+                <h4>
+                    <GroupWorkIcon fontSize='large' style={{ 'margin-right': '5px' }}/>
+                    CollabConnect
+                </h4>
+                {user ?
+                    <>
+                        <p>{`User: ${user.name}`}</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                    : <NavLink to='/login'>
+                        <button><LoginIcon fontSize='large'/></button>
+                    </NavLink>
+                }
+            </div>
             {children}
         </div>
     )
